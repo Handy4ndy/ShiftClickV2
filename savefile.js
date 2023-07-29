134490d3e2b3332a651344b05a1d9e92fe2dac2d
Andy <110458000+Handy4ndy@users.noreply.github.com>
2023-07-29 09:18:16 +0100

    update photoapp

---
+++ savefile.js
@@ -0,0 +1,41 @@
+let saveFile = () => {
+
+    //This is setting the checkboxes as an array
+    var form = document.getElementById('app');
+    var myArray = [];
+    form.querySelectorAll('input').forEach(function (input) {
+        if (input.type === 'checkbox' && input.checked) {
+            myArray.push(input.value)
+        }
+    })
+
+    form = document.forms.app.elements;
+    const gender = form.gender.value;
+    const personality = myArray;
+    const link = form.photourl.value;
+
+    // This variable stores all the data.
+    let data =
+        '\r Gender?: ' + gender + ' \r\n ' +
+        'Personality: ' + personality + ' \r\n ' +
+        'Link: ' + link
+
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = "filename";	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+    newLink.innerHTML = "Download File:";
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click();
+}
