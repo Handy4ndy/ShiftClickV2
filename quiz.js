bc469f198343556f4d1064757e88a0f5df5c1528
Andy <110458000+Handy4ndy@users.noreply.github.com>
2023-07-29 01:16:12 +0100

    quiz change

--- quiz.js
+++ quiz.js
@@ -12,39 +12,43 @@ function checkAnswers() {
 
     quiz = document.forms.Quiz.elements;
 
-    answer1 = quiz.html.value
+    answer1 = quiz.html.value;
 
     if (answer1 == "04") {
 
         score = score + 2;
     }
 
-    answer2 = quiz.tags.value
+    answer2 = quiz.tags.value;
 
     if (answer2 == "01") {
 
         score = score + 2;
     }
 
-    answer3 = quiz.css.value
+    answer3 = quiz.css.value;
 
     if (answer3 == "03") {
 
         score = score + 2;
     }
 
-    answer4 = quiz.java.value
+    answer4 = quiz.java.value;
 
     if (answer4 == "02") {
 
         score = score + 2;
     }
 
-    if (score <=2){
-    alert(`Unlucky ${username.value}, your score was ${score}`)
-    }else if(score <=6){
+    if (score <= 2){
+        alert(`Unlucky ${username.value}, your score was ${score}`)
+    }
+    
+    if(score <= 6){
         alert(`Nice work ${username.value}, your score is ${score}`)
-    }else(score >6);{
+    }
+    
+    if(score == 8);{
         alert(`Well done ${username.value} you Smashed it !! your score is ${score}`)
     }
 }
